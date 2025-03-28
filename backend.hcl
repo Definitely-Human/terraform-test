24862fc7edd3ee4d19afc5e4d2da0e4031dcd95e
Alexander <odest66@gmail.com>
2025-03-28 21:01:46 +0100

    Test commit

--- backend.hcl
+++
@@ -1,2 +0,0 @@
-workspaces { name = "Name of your terraform cloud workspace" }
-organization = "Name of your terraform cloud organization"
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   # To use S3 as the remote backend, comment out the below line
-  backend "remote" {}
+  #backend "remote" {}
 
   # To use S3 as the backend, uncomment the below line
   #backend "s3" {}
@@ -17,7 +17,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Sample module that provisions an AWS API Gateway. Replace with the module that configures the specific AWS resource you wish to provision 
+# Sample module that provisions an AWS API Gateway. Replace with the module that configures the specific AWS resource you wish to provision
 module "http_api_gateway" {
   source                       = "./modules/http-api-gateway"
   http_api_gateway_name        = var.http_api_gateway_name
